0ee5c153d21284f01652c02a15ab261cd1d7ed9c
aeonviper <viperblaster@gmail.com>
2023-12-07 06:53:38 +0700

    Better monitoring

--- src/epsilon/controller/MonitorController.java
+++ src/epsilon/controller/MonitorController.java
@@ -32,52 +32,72 @@ public class MonitorController extends BaseController {
 			@Parameter("token") String token, //
 			@Parameter("action") String action, //
 			@Parameter("drive") String drive, //
+			@Parameter("path") String path, //
 			@Parameter("thresholdSize") Long thresholdSize, //
 			@Parameter("thresholdPercentage") Integer thresholdPercentage //
 	) {
 		if ("epsilon".equals(token)) {
-			if ("list".equals(action)) {
-				StringBuilder sb = new StringBuilder();
-				for (File file : File.listRoots()) {
-					sb.append(file.toString() + " - drive: " + hex(file.toString()) + " total:" + file.getTotalSpace() + " usable:" + file.getUsableSpace() + " free:" + file.getFreeSpace() + "\n");
+			if ("show".equals(action)) {
+				File file = null;
+				String text = "";
+				if (path != null) {
+					file = new File(path);
+				} else if (drive != null) {
+					file = new File(hexToString(drive));
 				}
-				return new View(Type.TEXT_PLAIN, sb.toString());
+				if (file != null && file.canRead()) {
+					text = file.toString() + " total:" + file.getTotalSpace() + " usable:" + file.getUsableSpace() + " free:" + file.getFreeSpace() + " " + ((file.getFreeSpace() * 100) / file.getTotalSpace() + "%");
+				}
+				return new View(Type.JSON, text);
 			} else if ("check".equals(action)) {
 				if (thresholdPercentage == null || thresholdPercentage < 0) {
 					thresholdPercentage = 20;
 				}
-				for (File file : File.listRoots()) {
-					if (hex(file.toString()).equals(drive)) {
-						if (thresholdSize != null) {
-							long value = file.getFreeSpace();
-							if (value < thresholdSize) {
-								return new View(Type.TEXT_PLAIN, "Error: threshold reached - " + file.toString() + " - total:" + file.getTotalSpace() + " usable:" + file.getUsableSpace() + " free:" + file.getFreeSpace() + " threshold:" + thresholdSize + " value:" + value);
-							} else {
-								return new View(Type.TEXT_PLAIN, "OK - " + file.toString() + " - total:" + file.getTotalSpace() + " usable:" + file.getUsableSpace() + " free:" + file.getFreeSpace() + " threshold:" + thresholdSize + " value:" + value);
-							}
-						}
-						if (thresholdPercentage != null) {
-							int value = (int) Math.floor((double) file.getFreeSpace() * 100 / file.getTotalSpace());
-							if (value < thresholdPercentage) {
-								return new View(Type.TEXT_PLAIN, "Error: threshold reached - " + file.toString() + " - total:" + file.getTotalSpace() + " usable:" + file.getUsableSpace() + " free:" + file.getFreeSpace() + " threshold:" + thresholdPercentage + " value:" + value);
-							} else {
-								return new View(Type.TEXT_PLAIN, "OK - " + file.toString() + " - total:" + file.getTotalSpace() + " usable:" + file.getUsableSpace() + " free:" + file.getFreeSpace() + " threshold:" + thresholdPercentage + " value:" + value);
-							}
-						}
-						return new View(Type.TEXT_PLAIN, "Error: no threshold to check");
-					}
+				File file = null;
+				if (path != null) {
+					file = new File(path);
+				} else if (drive != null) {
+					file = new File(hexToString(drive));
 				}
-				return new View(Type.TEXT_PLAIN, "Error: could not find drive");
+				if (file != null && file.canRead()) {
+					if (thresholdSize != null) {
+						long value = file.getFreeSpace();
+						if (value < thresholdSize) {
+							return new View(Type.JSON, "Error: threshold reached - " + file.toString() + " - total:" + file.getTotalSpace() + " usable:" + file.getUsableSpace() + " free:" + file.getFreeSpace() + " threshold:" + thresholdSize + " value:" + value);
+						} else {
+							return new View(Type.JSON, "OK - " + file.toString() + " - total:" + file.getTotalSpace() + " usable:" + file.getUsableSpace() + " free:" + file.getFreeSpace() + " threshold:" + thresholdSize + " value:" + value);
+						}
+					}
+					if (thresholdPercentage != null) {
+						int value = (int) Math.floor((double) file.getFreeSpace() * 100 / file.getTotalSpace());
+						if (value < thresholdPercentage) {
+							return new View(Type.JSON, "Error: threshold reached - " + file.toString() + " - total:" + file.getTotalSpace() + " usable:" + file.getUsableSpace() + " free:" + file.getFreeSpace() + " threshold:" + thresholdPercentage + " value:" + value);
+						} else {
+							return new View(Type.JSON, "OK - " + file.toString() + " - total:" + file.getTotalSpace() + " usable:" + file.getUsableSpace() + " free:" + file.getFreeSpace() + " threshold:" + thresholdPercentage + " value:" + value);
+						}
+					}
+					return new View(Type.JSON, "Error: no threshold to check");
+				}
+				return new View(Type.JSON, "Error: could not find path");
 			} else {
-				return new View(Type.TEXT_PLAIN, "Error: unknown action");
+				return new View(Type.JSON, "Error: unknown action");
 			}
 		} else {
-			return new View(Type.TEXT_PLAIN, "Error: wrong token");
+			return new View(Type.JSON, "Error: wrong token");
 		}
 	}
 
-	protected String hex(String text) {
+	public static String stringToHex(String text) {
 		return String.format("%x", new BigInteger(1, text.getBytes()));
+	}
+
+	public static String hexToString(String hex) {
+		int length = hex.length();
+		byte[] data = new byte[length / 2];
+		for (int i = 0; i < length; i += 2) {
+			data[i / 2] = (byte) ((Character.digit(hex.charAt(i), 16) << 4) + Character.digit(hex.charAt(i + 1), 16));
+		}
+		return new String(data);
 	}
 
 }
